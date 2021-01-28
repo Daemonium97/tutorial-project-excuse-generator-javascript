086661207749a67fc515bda1875576dee41b6490
Daemonium97 <daemonium97@gmail.com>
2021-01-28 01:27:16 +0000

    fixed bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'][Math.floor(Math.random() * 4)];
-let action = ['ate', 'peed', 'crushed', 'broke'][Math.floor(Math.random() * 4)];
-let what = ['my homework', 'the keys', 'the car'][Math.floor(Math.random() * 3)];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'][Math.floor(Math.random() * 3)];
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'][Math.floor(Math.random() * who.lenght)];
+let action = ['ate', 'peed', 'crushed', 'broke'] [Math.floor(Math.random() * action.lenght)];
+let what = ['my homework', 'the keys', 'the car'][Math.floor(Math.random() * what.lenght)];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'][Math.floor(Math.random() * when.lenght)];
 
 
 document.querySelector('#excuse').innerHTML = who + ' ' + action + ' ' + what + ' ' + when;
