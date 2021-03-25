9e051d0eca32ec8d7a6d4e3a319e6ba79bfadb22
Daemonium97 <daemonium97@gmail.com>
2021-03-25 06:39:45 +0000

    Fixing

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'][Math.floor(Math.random() * who.lenght)];
-let action = ['ate', 'peed', 'crushed', 'broke'] [Math.floor(Math.random() * action.lenght)];
-let what = ['my homework', 'the keys', 'the car'][Math.floor(Math.random() * what.lenght)];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'][Math.floor(Math.random() * when.lenght)];
+window.onload = () => {
+
+    document.querySelector('#excuse').innerHTML = generate();
+}
 
 
-document.querySelector('#excuse').innerHTML = who + ' ' + action + ' ' + what + ' ' + when;
+const generate = () => {
+
+    let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+
+    let whoExcuse = Math.floor(Math.random() * who.length);
+    let actionExcuse = Math.floor(Math.random() * action.length);
+    let whatExcuse = Math.floor(Math.random() * what.length);
+    let whenExcuse = Math.floor(Math.random() * when.length);
+
+    return who[whoExcuse] + ' ' + action[actionExcuse] + ' ' + what[whatExcuse] + ' ' + when[whenExcuse];
+
+}
+
